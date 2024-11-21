071b5da832697eb73076fc008fd17a442eb6308c
dinhvandong <dongvan.ict3@gmail.com>
2024-11-21 20:03:34 +0700

    update new code

---
+++ SaleManagementWinform/SaleManagementWinform/CustomerForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace SaleManagementWinform
+{
+    partial class CustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_customer = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_customer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_customer
+            // 
+            this.dgv_customer.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_customer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_customer.Location = new System.Drawing.Point(0, 0);
+            this.dgv_customer.Name = "dgv_customer";
+            this.dgv_customer.RowHeadersWidth = 51;
+            this.dgv_customer.RowTemplate.Height = 24;
+            this.dgv_customer.Size = new System.Drawing.Size(800, 450);
+            this.dgv_customer.TabIndex = 0;
+            this.dgv_customer.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_customer_CellContentClick);
+            // 
+            // CustomerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv_customer);
+            this.Name = "CustomerForm";
+            this.Text = "CustomerForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_customer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_customer;
+    }
+}
---
+++ SaleManagementWinform/SaleManagementWinform/CustomerForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SaleManagementWinform
+{
+    public partial class CustomerForm : Form
+    {
+
+        public static string connectionString
+          = "Server=DONGVANDINH\\MISASME2023;Database=SALE_MANGEMENT_DB;Trusted_Connection=True;";
+
+
+        public CustomerForm()
+        {
+            InitializeComponent();
+
+            this.WindowState = FormWindowState.Maximized;
+
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+
+            // Optionally, set StartPosition to CenterScreen if you want centered loading
+            this.StartPosition = FormStartPosition.CenterScreen;
+            dgv_customer.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            this.LoadData();
+        }
+
+        private void dgv_customer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+
+        private void LoadData()
+        {
+            // SQL query to fetch data
+            string query = "SELECT * FROM Customer";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // Open the database connection
+                    connection.Open();
+
+                    // Create a SqlDataAdapter to execute the query and fill the DataTable
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                    DataTable dataTable = new DataTable();
+
+                    // Fill the DataTable with query results
+                    adapter.Fill(dataTable);
+
+                    // Bind the DataTable to the DataGridView
+                    dgv_customer.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    // Handle any errors that may occur
+                    MessageBox.Show("An error occurred: " + ex.Message);
+                }
+            }
+        }
+    }
+}
